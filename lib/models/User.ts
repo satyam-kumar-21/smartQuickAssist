832f75729526a0f32a1618092888cbb8ed056e36
Satyam kumar <satyamkumar2716@gmail.com>
2026-06-23 04:50:21 +0530

    updated

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -18,13 +18,14 @@ userSchema.methods.matchPassword = async function (enteredPassword: string) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.pre('save', async function (next) {
+// FIX: Explicitly type the next middleware function signature
+// Remove the 'next' argument completely from the async function
+userSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        return next();
+        return; // Just return instead of calling next()
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
